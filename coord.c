5ddccb2f46d97c5912200b51c86f80e9e0d00c80
marbel <othmane.xx90@gmail.com>
2024-11-10 10:53:47 +0100

    typedef struct

--- coord.c
+++ coord.c
@@ -1,11 +1,17 @@
 #include "coord.h"
 
-struct Coordinate new_coord(int x, int y, int z) {
-  struct Coordinate c = {
-    .x = x;
-    .y = y;
-    .z = z;
-  };
-  return c;
+coordinate_t new_coord(int x, int y, int z) {
+  coordinate_t coord = {.x = x, .y = y, .z = z};
+
+  return coord;
+}
+
+coordinate_t scale_coordinate(coordinate_t coord, int factor) {
+  coordinate_t scaled = coord;
+  scaled.x *= factor;
+  scaled.y *= factor;
+  scaled.z *= factor;
+
+  return scaled;
 }
 
--- coord.h
+++ coord.h
@@ -1,8 +1,11 @@
 #pragma once
 
-struct Coordinate {
+typedef struct Coordinate {
   int x;
   int y;
   int z;
-};
+} coordinate_t;
+
+coordinate_t new_coord(int x, int y, int z);
+coordinate_t scale_coordinate(coordinate_t coord, int factor);
 
