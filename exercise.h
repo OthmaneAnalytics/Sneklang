8ddfec8547e005b34fca1684e22d4a660a61b71d
marbel <othmane.xx90@gmail.com>
2024-11-12 15:58:52 +0100

    stack overview

--- exercise.h
+++ exercise.h
@@ -1,10 +1,5 @@
-#include <stdint.h>
+void printMessageOne();
+void printMessageTwo();
+void printMessageThree();
+void printStackPointerDiff();
 
-typedef union PacketHeader{
-  struct {
-    uint8_t src_port;
-    uint8_t dest_port;
-    uint16_t seq_num;
-    } tcp_header;
-    uint32_t raw;
-} packet_header_t;
