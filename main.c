ecb3acac0c4c9fa44d729408d5973fd5162f97be
marbel <othmane.xx90@gmail.com>
2024-11-12 16:13:23 +0100

    stack overflow

--- main.c
+++ main.c
@@ -1,44 +1,16 @@
 #include <stdio.h>
-#include "exercise.h"
 
 int main() {
-  printMessageOne();
-  printMessageTwo();
-  printMessageThree();
+  const int pool_size = 1024 * 10;
+  char snek_pool[pool_size];
+  snek_pool[0] = 's';
+  snek_pool[1] = 'n';
+  snek_pool[2] = 'e';
+  snek_pool[3] = 'k';
+  snek_pool[4] = '\0';
+
+  printf("Size of pool: %d\n", pool_size);
+  printf("Initial string: %s\n", snek_pool);
   return 0;
-}
-
-void printMessageOne() {
-  const char *message = "Dark mode?\n";
-  printStackPointerDiff();
-  printf("%s\n", message);
-}
-
-void printMessageTwo() {
-  const char *message = "More like...\n";
-  printStackPointerDiff();
-  printf("%s\n", message);
-}
-
-void printMessageThree() {
-  const char *message = "dark roast.\n";
-  printStackPointerDiff();
-  printf("%s\n", message);
-}
-
-// don't touch below this line
-
-void printStackPointerDiff() {
-  static void *last_sp = NULL;
-  void *current_sp;
-  current_sp = __builtin_frame_address(0);
-  long diff = (char*)last_sp - (char*)current_sp;
-  if (last_sp == NULL){
-    last_sp = current_sp;
-    diff = 0;
-  }
-  printf("---------------------------------\n");
-  printf("Stack pointer offset: %ld bytes\n", diff);
-  printf("---------------------------------\n");
 }
 
