da4788120cb91c353b3e5007354ff2da61f2de28
marbel <othmane.xx90@gmail.com>
2024-11-15 21:59:13 +0100

    vector 3 start

--- snekobject.h
+++ snekobject.h
@@ -1,15 +1,23 @@
+typedef struct snek_object_t;
 
+typedef struct {
+  sneak_object_t* x;
+  sneak_object_t* y;
+  sneak_object_t* z;
+}snek_vector_t;
 
 typedef enum {
   INTEGER,
   FLOAT,
   STRING,
+  VECTOR3,
 } snek_object_kind_t;
 
 typedef union {
   int v_int;
   float v_float;
   char* v_string;
+  sneak_vector_t* v_vector3;
 } snek_object_data_t;
 
 typedef struct{
