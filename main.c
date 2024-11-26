189df72537c75143cc63f2c461275579dd43c7ad
marbel <othmane.xx90@gmail.com>
2024-11-26 12:29:08 +0100

    Free

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 
 #include "sneknew.h"
-#include "snekobject.h"
 #include "vm.h"
 
 int main() {
   vm_t *vm = vm_new();
-  snek_object_t *obj = new_snek_integer(vm, 5);
+  new_snek_integer(vm, 5);
+  new_snek_string(vm, "hello");
   vm_new_frame(vm);
-  free(obj);
   vm_free(vm);
 
   return 0;
